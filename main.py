aa72b6c8340336ef9840acc3fbfb452777316727
Daniel Cárdenas Rodríguez <daniel24cardenasr@gmail.com>
2024-12-03 22:32:53 -0500

    Se agrega despliegue automatico usando GitHub Actions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import os
 from fastapi import FastAPI
 from redis import Redis
 import datetime
 
+REDIS_URL = os.getenv('REDIS_URL')
 # Create a database connection
 try:
-    redis_connection = Redis("db")
+    redis_connection = Redis.from_url(REDIS_URL)
 except ConnectionError as e:
     print("error:", e)
 
@@ -18,7 +20,7 @@ def get_last_connection():
     
     # Get current time
     current_time = datetime.datetime.now()
-    
+
     # Set time connection in database
     redis_connection.set("last-connection", str(current_time))
 
